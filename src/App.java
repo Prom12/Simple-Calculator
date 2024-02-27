553ffaa5ecd40f6935f5e4db93068df7df752590
Prom <51293420+Prom12@users.noreply.github.com>
2024-02-27 22:15:28 +0000

    Initial comment

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //  Add, Substract, Multiply, Divide
+        //  I need at least two numbers
+        //  
+    }
+}
